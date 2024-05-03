b9c7701cd54051dda22563aa7ca5a4d177a07ffb
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-05-03 14:29:31 +0800

    Update Week3

--- Week3/books-api-mvc/app.js
+++ Week3/books-api-mvc/app.js
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
 // Define individual routes for each controller function
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
-app.post("/books", booksController.createBook);
+app.post("/books", validateBook, booksController.createBook);
 app.put("/books/:id", validateBook, booksController.updateBook);
-app.delete("/books/:id", validateBook, booksController.deleteBook);
+app.delete("/books/:id", booksController.deleteBook);
 
 const port = process.env.PORT || 3000;
 
