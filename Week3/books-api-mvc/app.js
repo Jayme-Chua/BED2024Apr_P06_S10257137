27b6e603214e9631b08e4da847725e38fb779189
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-05-03 13:45:10 +0800

    Completed Week 3 task & OAL

---
+++ Week3/books-api-mvc/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("./controllers/booksController"); // Import controllers
+const validateBook = require("./middlewares/validateBook");
+const app = express();
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+// Define individual routes for each controller function
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", booksController.createBook);
+app.put("/books/:id", validateBook, booksController.updateBook);
+app.delete("/books/:id", validateBook, booksController.deleteBook);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
